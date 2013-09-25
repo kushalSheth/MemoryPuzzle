e6049095f0cc9e5aad11e7dd60b57461cc1d8ab8
kushalSheth <kushalsheth14@gmail.com>
2013-09-25 05:14:29 -0700

    Commit 5

--- js/app.js
+++ js/app.js
@@ -2,64 +2,41 @@
 var app = app || {};
 
 $(function() {
-    var tiles = [
-			{
-				"image" : "https://encrypted-tbn0.gstatic.com/images?q = t b n : A Nd9GcRHQ5Sf2Cffo0QdRtFw0t8zdk90FWgywc1kDKAuOV874zYO_0pC",
-				"phrase" : "Ra.One"
-			},
-			{
-				"image" : "https://encrypted-tbn3.gstatic.com/images?q = t b n : A Nd9GcQkVg9n2UbPTeiPNGUOw2SqdAzJsZzuHrkYw78sjaMMCcTWcuFjUtT3NZ8",
-				"phrase" : "3 Idiots"
-			},
-			{
-				"image" : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ0Wam0d96ASpEHDFfskGTBdILKlDpXAKzELFdfdKmkoye5JmrF9qS1u1qtZw",
-				"phrase" : "Chaalis Chauraasi (4084)"
-			},
-			{
-				"image" : "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSAf7q-m96JL88_W8EATmVlwC fO K b 5 d w 1keyDSO6PWwzaSVtqGyix2lvE",
-				"phrase" : "Gangs Of Wasseypur"
-			},
-			{
-				"image" : "https://encrypted-tbn1.gstatic.com/images?q = t b n : A Nd9GcT0s8gwKiTiGlA7ReE0X9_Etm8a2lhEGUD_j4a2VrLZSZuBNeviGRoRrNs",
-				"phrase" : "Jodi Breakers"
-			},
-			{
-				"image" : "https://encrypted-tbn0.gstatic.com/images?q = t b n : A Nd9GcQogG0I0JlOnGEstbajO_58Az4lOR7TAdnGhP1BtvhcPUgl2q1I5Q",
-				"phrase" : "Kahaani"
-			},
-			{
-				"image" : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRHVQ5HkvH3EY3rGmpDFzPTz2JO6lKWh6oIA6qy-5KAAvF4VKX2fcuCxyE",
-				"phrase" : "Ekk Main Aur Ekk Tu"
-			},
-			{
-				"image" : "https://encrypted-tbn1.gstatic.com/images?q = t b n : A Nd9GcT8oL0tVOPZvi9lCopFfuxDv4U0w80trjT9D1_zcMiRsPMbYPXi8w",
-				"phrase" : "Barfii"
-			},
-			{
-				"image" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMSR6-r i N i Z a q JB9W8l8ka1tCH87uEXF1mVgBqoUxSzDEGPTL2",
-				"phrase" : "Talaash"
-			},
-			{
-				"image" : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcT8C4ytjUo-5xeUS3olEg4LCz8N7ehLFW7kPIVX5ljg34dsdBdd",
-				"phrase" : "Agneepath"
-			},
-			{
-				"image" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQH6XO3G5uqhYysFcB6-B E m w V f W MNtVnxiJBiOxHg9nIRpxIZMiA",
-				"phrase" : "Rowdy Rathore"
-			},
-			{
-				"image" : "https://encrypted-tbn3.gstatic.com/images?q = t b n : A Nd9GcR_C04HmnJWajxV2ITIo0GeJaPOMqYPLZOFKyQ7ugpNe9RTsuwshg",
-				"phrase" : "Bhoot"
-			},
-			{
-				"image" : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRP7QaUPfL7r4aHfYs5dTP24r6IA5zjgLg8NZK4VV3e1Uhoz67_",
-				"phrase" : "Ek Tha Tiger"
-			},
-			{
-				"image" : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcS2T-4W0nJgnDE0B74TXTXADDB_0rBc6FvyoHE2xQsRV4GJak31g",
-				"phrase" : "Tezz"
-			}    
-	];
-
-    new app.AppView(tiles);
+	
+	var tiles = [];
+	var tilesNum = getRandomTileNum(2,4);
+	
+	$.ajax({
+		url : 'js/ajax/words.json',
+		dataType : 'json',
+		success : function(response){
+			response.data.splice(tilesNum);
+			tiles = response.data.concat(response.data); 
+			tiles = shuffleTiles(tiles);
+			alert(tiles.length);
+			new app.AppView(tiles);
+		},
+		error : function(){
+			$("#errorMessage").html("Failed to load JSON!").show(0);
+		}
+	});
+	
 });
+
+function getRandomTileNum (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function shuffleTiles(tiles){
+	var noOfSwaps = getRandomTileNum((tiles.length/3), tiles.length);
+	var i1=0;
+	var i2=0;
+	for(var i=0;i<noOfSwaps;i++){
+		i1= getRandomTileNum(0, tiles.length-1);
+		i2 = getRandomTileNum(0, tiles.length-1);
+		var temp = tiles[i1];
+		tiles[i1] = tiles[i2];
+		tiles[i2] = temp;
+	}
+	return tiles;
+}
--- js/views/mainView.js
+++ js/views/mainView.js
@@ -9,11 +9,43 @@ app.AppView = Backbone.View.extend({
         this.render();
     },
 
-    // render library by rendering each book in its collection
+    events:{
+        'click .tileBackground':'startGame'
+    },
+
+    startGame: function(e) {
+    	var $this = $(e.target);
+    	var curView = this;
+    	var phrase = $(".tileImg:visible").attr('alt');
+    	$this.hide(0).parent().children(".tileImg").show(0);
+    	var curPhrase = $this.parent().children(".tileImg").attr('alt');
+    	
+    	if(phrase){
+    		$("#disableDiv").show(0);
+    		setTimeout(function(){
+	    		if(phrase === curPhrase){
+	    			$(".tileImg:visible").parent().children(".tileBackground").attr('flag','true');
+	    			$(".tileImg:visible").hide(0).parent().children(".tilePhrase").show(0);
+	    			$("#messageDiv").html("Removed matching Tiles!").show(0).delay(3000).hide(0);
+	    			if($(".tileBackground").filter('[flag="false"]').length == 0){
+	    				$("#completeMsg").show(0);
+	    			}
+	    		}else{
+	    			$(".tileImg:visible").hide(0);
+	    			$(".tileBackground").filter('[flag="false"]').show(0);
+	    			$("#messageDiv").html("No matching Tiles!").show(0).delay(3000).hide(0);
+	    		}
+	    		$("#disableDiv").hide(0);
+	    	},1000);
+    	}
+    	
+    },
+
     render: function() {
         this.collection.each(function( item ) {
             this.renderTile( item );
         }, this );
+        this.$el.append("<div style='clear:both'></div>");
     },
 
     renderTile : function( item ) {
